afed9834d4dc40c8faa9ede52133c1ebd27dc0a2
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-06-29 23:21:59 +0200

    fix: update paths used by cli

--- generate-from-schema/dist/index.d.ts
+++ generate-from-schema/dist/index.d.ts
@@ -7,4 +7,39 @@ declare const convertToNetlifyCMS: any;
 declare const Ajv: any;
 declare const ajv: any;
 declare const ignoredFormats: string[];
+declare const pageSchema: {
+    $schema: string;
+    $id: string;
+    title: string;
+    description: string;
+    type: string;
+    required: string[];
+    properties: {
+        id: {
+            type: string;
+            title: string;
+            description: string;
+            format: string;
+        };
+        layout: {
+            type: string;
+            title: string;
+            description: string;
+            default: string;
+        };
+        title: {
+            type: string;
+            title: string;
+            description: string;
+        };
+        sections: {
+            type: string;
+            title: string;
+            description: string;
+            items: {
+                $ref: string;
+            };
+        };
+    };
+};
 declare const addSchema: (schemaPath: string) => Promise<any>;

b5f28b67c038ae0e44ecf57c5d5efcbd9aeb9f42
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-07-11 12:42:14 +0200

    fix: interface File, and schema query removed

--- graphql-cli-add-gatsby-directives/src/index.ts
+++ graphql-cli-add-gatsby-directives/src/index.ts
@@ -2,5 +2,7 @@ import { PluginFunction } from '@graphql-codegen/plugin-helpers';
 import { augment } from './components/augment';
 
 export const plugin: PluginFunction<any> = (schema) => {
-  return augment(schema) + '\n\n';
+  return (augment(schema) + '\n\n')
+    .replace('interface File', '')
+    .replace('schema {\n  query: Query\n}\n\n', '');
 };

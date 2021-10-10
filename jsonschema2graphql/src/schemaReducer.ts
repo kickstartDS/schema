d8a2ccb7724a8b3375d20df00cbf41b5fc5cb51b
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2021-10-10 21:23:42 +0200

    feature: move more hashing logic into schema tools

--- jsonschema2graphql/src/schemaReducer.ts
+++ jsonschema2graphql/src/schemaReducer.ts
@@ -60,10 +60,92 @@ export function cleanFieldName(name: string): string {
   return name.replace(/__.*/i, '');
 };
 
+export function cleanObjectKeys(obj: Record<string, any>) {
+  const cleanedObject = {};
+
+  Object.keys(obj).forEach((property) => {
+    if (property !== typeResolutionField) {
+      if (Array.isArray(obj[property])) {
+        if (obj[property].length > 0) {
+          cleanedObject[cleanFieldName(property)] = obj[property].map((item: Record<string, any>) => {
+            return cleanObjectKeys(item);
+          });
+        }
+      } else if (typeof obj[property] === 'object') {
+        if (obj[property] !== null) {
+          cleanedObject[cleanFieldName(property)] =
+            cleanObjectKeys(obj[property]);
+        }
+      } else if (obj[property]) {
+        if (obj[property] !== null) {
+          // TODO re-simplify this... only needed because of inconsistent handling of `-` vs `_` in schema enum values
+          // TODO also `graphqlSafeEnumKey.ts` is destructive right now, as in: you can't deterministically convert
+          // values back to their original form, once they are made safe. This is why different properties (like `ratio`
+          // or `pattern`) need to be handled explicitly here. To reconstruct the needed format. As properties can be
+          // customized from a project-level (e.g. `pattern` already is an individualization for `kickstartDS/design-system`)
+          // we can't have custom handling per property here. At least in the long run!
+          if (cleanFieldName(property) === 'variant') {
+            cleanedObject[cleanFieldName(property)] = obj[property].replace('_', '-');
+          } else if (cleanFieldName(property) === 'ratio') {
+            cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '').replace('_', ':');
+          } else if (cleanFieldName(property) === 'pattern') {
+            cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '');
+          } else {
+            cleanedObject[cleanFieldName(property)] = obj[property];
+          }
+        }
+      }
+    }
+  });
+
+  return cleanedObject;
+};
+
 export function hashFieldName(fieldName: string, optionalName?: string): string {
   return fieldName.includes('___NODE')
     ? `${fieldName.replace('___NODE', '')}__${createHash('md5').update(fieldName.replace('___NODE', '') + (optionalName || '')).digest('hex').substr(0,4)}___NODE`
     : `${fieldName}__${createHash('md5').update(fieldName + (optionalName || '')).digest('hex').substr(0,4)}`;
+};
+
+export function hashObjectKeys(obj: Record<string, any>, outerComponent: string) {
+  const hashedObj = {};
+
+  if (!obj) return obj;
+
+  Object.keys(obj).forEach((property) => {
+    if (property === typeResolutionField) {
+      hashedObj[typeResolutionField] = obj[typeResolutionField];
+    } else {
+      if (Array.isArray(obj[property])) {
+        hashedObj[hashFieldName(property, outerComponent)] = obj[property].map((item: Record<string, any>) => {
+          // TODO re-simplify this... only needed because of inconsistent hashing on sub-types / picture
+          if (outerComponent === 'logo-tiles') {
+            return hashObjectKeys(item, 'picture');
+          } else if (outerComponent === 'quotes-slider') {
+            return hashObjectKeys(item, 'quote');
+          } else if (outerComponent === 'post-head' && property === 'categories') {
+            return hashObjectKeys(item, 'tag-label');
+          } else {
+            return hashObjectKeys(item, outerComponent === 'section' ? item[typeResolutionField] : outerComponent);
+          }
+        });
+      } else if (typeof obj[property] === 'object') {
+        // TODO re-simplify this... only needed because of inconsistent hashing on sub-types / link-button
+        const outer = outerComponent === 'section' ? obj[property][typeResolutionField] : outerComponent;
+        if (outer === 'storytelling' && property === 'link') {
+          hashedObj[hashFieldName(property, outerComponent)] = hashObjectKeys(obj[property], 'link-button');
+        } else if (outer === 'storytelling' && property === 'headline') {
+          hashedObj[hashFieldName(property, outerComponent)] = hashObjectKeys(obj[property], 'headline');
+        } else {
+          hashedObj[hashFieldName(property, outerComponent)] = hashObjectKeys(obj[property], outer);
+        }
+      } else {
+        hashedObj[hashFieldName(property, outerComponent === 'section' ? 'section' : outerComponent)] = obj[property];
+      }
+    }
+  });
+
+  return hashedObj;
 };
 
 export function getSchemaName(schemaId: string | undefined): string {

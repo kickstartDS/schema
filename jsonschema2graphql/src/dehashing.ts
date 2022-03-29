aabb844d35f1e135c89aea74d09c8b2254e7ae98
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-03-29 20:44:12 +0200

    fix: tina typename

--- jsonschema2graphql/src/dehashing.ts
+++ jsonschema2graphql/src/dehashing.ts
@@ -22,8 +22,12 @@ export function cleanObjectKeys(obj: Record<string, any>): Record<string, any> {
         }
       } else if (typeof obj[property] === 'object') {
         if (obj[property] !== null) {
-          cleanedObject[cleanFieldName(property)] =
-            cleanObjectKeys(obj[property]);
+          if (obj[property]?.type !== 'root') {
+            cleanedObject[cleanFieldName(property)] =
+              cleanObjectKeys(obj[property]);
+          } else {
+            cleanedObject[cleanFieldName(property)] = obj[property];
+          }
         }
       } else if (obj[property]) {
         if (obj[property] !== null) {
@@ -39,6 +43,8 @@ export function cleanObjectKeys(obj: Record<string, any>): Record<string, any> {
             cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '').replace('_', ':');
           } else if (cleanFieldName(property) === 'pattern') {
             cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '');
+          } else if (property === '__typename') {
+            cleanedObject[property] = obj[property];
           } else {
             cleanedObject[cleanFieldName(property)] = obj[property];
           }

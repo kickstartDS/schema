f0d6bae2de0d1e75018916ac4cea19b22c930e12
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-08-15 15:50:55 +0200

    fix: bug in hashing / de-hashing

--- jsonschema2graphql/src/dehashing.ts
+++ jsonschema2graphql/src/dehashing.ts
@@ -30,27 +30,25 @@ export function cleanObjectKeys(obj: Record<string, any>): Record<string, any> {
             cleanedObject[cleanFieldName(property)] = obj[property];
           }
         }
-      } else if (obj[property]) {
-        if (obj[property] !== null) {
-          // TODO re-simplify this... only needed because of inconsistent handling of `-` vs `_` in schema enum values
-          // TODO also `graphqlSafeEnumKey.ts` is destructive right now, as in: you can't deterministically convert
-          // values back to their original form, once they are made safe. This is why different properties (like `ratio`
-          // or `pattern`) need to be handled explicitly here. To reconstruct the needed format. As properties can be
-          // customized from a project-level (e.g. `pattern` already is an individualization for `kickstartDS/design-system`)
-          // we can't have custom handling per property here. At least in the long run!
-          if (cleanFieldName(property) === 'variant') {
-            cleanedObject[cleanFieldName(property)] = obj[property].replace('_', '-');
-          } else if (cleanFieldName(property) === 'background') {
-            cleanedObject[cleanFieldName(property)] = obj[property].replace('_', '-');
-          } else if (cleanFieldName(property) === 'ratio') {
-            cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '').replace('_', ':');
-          } else if (cleanFieldName(property) === 'pattern') {
-            cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '');
-          } else if (property === '__typename') {
-            cleanedObject[property] = obj[property];
-          } else {
-            cleanedObject[cleanFieldName(property)] = obj[property];
-          }
+      } else if (obj[property] !== null) {
+        // TODO re-simplify this... only needed because of inconsistent handling of `-` vs `_` in schema enum values
+        // TODO also `graphqlSafeEnumKey.ts` is destructive right now, as in: you can't deterministically convert
+        // values back to their original form, once they are made safe. This is why different properties (like `ratio`
+        // or `pattern`) need to be handled explicitly here. To reconstruct the needed format. As properties can be
+        // customized from a project-level (e.g. `pattern` already is an individualization for `kickstartDS/design-system`)
+        // we can't have custom handling per property here. At least in the long run!
+        if (cleanFieldName(property) === 'variant') {
+          cleanedObject[cleanFieldName(property)] = obj[property].replace('_', '-');
+        } else if (cleanFieldName(property) === 'background') {
+          cleanedObject[cleanFieldName(property)] = obj[property].replace('_', '-');
+        } else if (cleanFieldName(property) === 'ratio') {
+          cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '').replace('_', ':');
+        } else if (cleanFieldName(property) === 'pattern') {
+          cleanedObject[cleanFieldName(property)] = obj[property].replace('VALUE_', '');
+        } else if (property === '__typename') {
+          cleanedObject[property] = obj[property];
+        } else {
+          cleanedObject[cleanFieldName(property)] = obj[property];
         }
       }
     }

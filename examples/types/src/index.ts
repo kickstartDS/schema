e424ffb3c6d2d754527174f9f7665ccefdbecf7b
Jonas Ulrich <jonas.ulrich@kickstartds.com>
2023-09-22 23:29:23 +0200

    chore: add ds-agency test to types example

--- examples/types/src/index.ts
+++ examples/types/src/index.ts
@@ -5,7 +5,8 @@ import {
   getCustomSchemaIds,
   getSchemaRegistry,
   getUniqueSchemaIds,
-  processSchemaGlob
+  processSchemaGlob,
+  shouldLayer
 } from '@kickstartds/jsonschema-utils';
 import { createTypes } from '@kickstartds/jsonschema2types';
 import { resolve } from 'import-meta-resolve';
@@ -13,7 +14,7 @@ import { resolve } from 'import-meta-resolve';
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const packagePath = path.dirname(
-    fileURLToPath(resolve(`@kickstartds/design-system/package.json`, import.meta.url))
+    fileURLToPath(resolve(`@kickstartds/ds-agency/package.json`, import.meta.url))
   );
   const customGlob = `${packagePath}/(dist|cms)/**/*.(schema|definitions).json`;
 
@@ -21,11 +22,15 @@ import { resolve } from 'import-meta-resolve';
   // set of unique schemas. precondition for the following conversions
   const ajv = getSchemaRegistry();
   const schemaIds = await processSchemaGlob(customGlob, ajv, false);
+  const kdsSchemaIds = schemaIds.filter((schemaId) => schemaId.includes('schema.kickstartds.com'));
 
   const customSchemaIds = getCustomSchemaIds(schemaIds);
-  const kdsSchemaIds = getUniqueSchemaIds(schemaIds).filter(
+  const unlayeredSchemaIds = getUniqueSchemaIds(schemaIds).filter(
     (schemaId) => !customSchemaIds.includes(schemaId)
   );
+  const layeredSchemaIds = customSchemaIds.filter((schemaId) =>
+    kdsSchemaIds.some((kdsSchemaId) => shouldLayer(schemaId, kdsSchemaId))
+  );
 
-  await createTypes([...kdsSchemaIds, ...customSchemaIds], ajv);
+  await createTypes([...unlayeredSchemaIds, ...layeredSchemaIds], kdsSchemaIds, ajv);
 })();

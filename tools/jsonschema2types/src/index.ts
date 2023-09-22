c6383c652345e03751a5a719e444ecd52cda514d
Jonas Ulrich <jonas.ulrich@kickstartds.com>
2023-09-22 23:29:44 +0200

    fix: layered type generation

--- tools/jsonschema2types/src/index.ts
+++ tools/jsonschema2types/src/index.ts
@@ -2,13 +2,19 @@ import { writeFileSync, mkdirSync } from 'node:fs';
 
 // import $RefParser from '@bcherny/json-schema-ref-parser';
 import { compile } from '@kickstartds/json-schema-to-typescript';
-import { getSchemaName, getSchemasForIds } from '@kickstartds/jsonschema-utils';
+import {
+  getSchemaName,
+  getSchemaModule,
+  getSchemasForIds,
+  isLayering,
+  layeredSchemaId
+} from '@kickstartds/jsonschema-utils';
 import { JSONSchema4 } from 'json-schema';
 import { pascalCase } from 'pascal-case';
 
 declare type MyAjv = import('ajv').default;
 
-export async function createTypes(schemaIds: string[], ajv: MyAjv): Promise<void> {
+export async function createTypes(schemaIds: string[], kdsSchemaIds: string[], ajv: MyAjv): Promise<void> {
   // const schemaDomain = 'kickstartds.com';
   // const kdsResolver: $RefParser.ResolverOptions = {
   //   canRead: new RegExp(`^http:\/\/schema\.kickstartds\.com`, 'i'),
@@ -28,9 +34,8 @@ export async function createTypes(schemaIds: string[], ajv: MyAjv): Promise<void
 
   mkdirSync('dist', { recursive: true });
   for (const schema of schemas) {
-    writeFileSync(`dist/${getSchemaName(schema.$id)}.schema.json`, JSON.stringify(schema, null, 2));
-
-    // console.log(`Title: ${schema.title}, $id: ${schema.$id}`);
+    if (!schema.$id) throw new Error("Can't process a schema without $id");
+    // writeFileSync(`dist/${getSchemaName(schema.$id)}.schema.json`, JSON.stringify(schema, null, 2));
 
     const typings = await compile(
       {
@@ -48,6 +53,18 @@ export async function createTypes(schemaIds: string[], ajv: MyAjv): Promise<void
       }
     );
 
-    writeFileSync(`dist/${pascalCase(getSchemaName(schema.$id))}Props.ts`, typings);
+    const layeredId = isLayering(schema.$id, kdsSchemaIds)
+      ? layeredSchemaId(schema.$id, kdsSchemaIds)
+      : schema.$id;
+
+    // console.log(layeredId, schema.$id, isLayering(schema.$id, kdsSchemaIds));
+
+    writeFileSync(
+      `dist/${pascalCase(getSchemaName(layeredId))}Props.ts`,
+      `declare module "@kickstartds/${getSchemaModule(layeredId)}/lib/${getSchemaName(layeredId)}/typing" {
+${typings}
+}
+    `
+    );
   }
 }

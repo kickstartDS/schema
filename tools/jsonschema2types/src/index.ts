3054b768aab1450f1dde053232d95cabd1654037
Jonas Ulrich <jonas.ulrich@kickstartds.com>
2025-02-28 15:53:55 +0100

    feat: make naming in generated types configurable

--- tools/jsonschema2types/src/index.ts
+++ tools/jsonschema2types/src/index.ts
@@ -5,25 +5,35 @@ import { pascalCase } from 'pascal-case';
 
 declare type MyAjv = import('ajv').default;
 
+export function defaultTitleFunction(schema: JSONSchema4): string {
+  return `${schema.title}Props`;
+}
+
+export function defaultFilenameFunction(schema: JSONSchema4): string {
+  return `${pascalCase(getSchemaName(schema.$id))}Props`;
+}
+
 export async function createTypes(
   schemaIds: string[],
   renderImportName: (schemaId: string) => string,
   renderImportStatement: (schemaId: string) => string,
   ajv: MyAjv,
-  options?: Partial<Options>
+  options?: Partial<Options>,
+  titleFunction: (schema: JSONSchema4) => string = defaultTitleFunction,
+  filenameFunction: (schema: JSONSchema4) => string = defaultFilenameFunction
 ): Promise<Record<string, string>> {
   const generatedTypings: Record<string, string> = {};
   const schemas = getSchemasForIds(schemaIds, ajv);
 
-  for (const schema of schemas) {
+  for (const schema of schemas as JSONSchema4[]) {
     if (!schema.$id) throw new Error("Can't process a schema without $id");
 
     const typings = await compile(
       {
-        ...(schema as JSONSchema4),
-        title: `${schema.title}Props`
+        ...schema,
+        title: titleFunction(schema)
       },
-      `${pascalCase(getSchemaName(schema.$id))}Props`,
+      filenameFunction(schema),
       {
         declareExternallyReferenced: true,
         $refOptions: {
